ccab4a65f2392b3eeed03dced74fde0466ffa5d2
parkyeongeon <yeongeon1003@gmail.com>
2023-05-07 23:44:17 +0900

    first commit

---
+++ io_practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0, y = 0, z = 0;
+    cout << "Hello, World!!!\a";
+    cin >> x >> y >> z;
+    cout << x << y << z;
+    return 0;
+}
